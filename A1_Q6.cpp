7a882db3edb5d7065b3404c383908f63c2630eca
manyakalra05 <146841072+manyakalra05@users.noreply.github.com>
2023-10-03 14:57:21 +0200

    Create A1_Q6.cpp

---
+++ A1_Q6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int n,k,s;
+    cout<<"Enter the no.";
+    cin>>n;
+    for(k=0; n>0; k++)
+    {
+      s=n%10;
+      cout<<r;
+      n=n/10;
+    }   
+  return 0;
+}
